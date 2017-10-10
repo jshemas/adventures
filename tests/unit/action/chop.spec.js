f51bd4bd2702cbb17eb8ce93620093c8ee5877b5
jshemas <joshshemas@gmail.com>
2017-10-09 21:45:39 -0400

    adding more chop tests

--- tests/unit/action/chop.spec.js
+++ tests/unit/action/chop.spec.js
@@ -32,6 +32,76 @@ describe('chop', function () {
       });
     });
   });
+  context('should be able to chop wood and not get a item', function () {
+    beforeEach(function () {
+      sandbox.stub(db, 'getUserInfo').resolves({
+        name: 'main',
+        level: 1,
+        experience: 1,
+        chopLevel: 3,
+        chopExperience: 2,
+        items: {
+          wood: 1
+        }
+      });
+      sandbox.stub(db, 'updateUser').resolves(true);
+      sandbox.stub(Math, 'random').returns(0);
+    });
+    it('was able to chop wood', function (done) {
+      chop.init().then(function (res) {
+        expect(res.success).to.eql(true);
+        expect(res.levelUp).to.eql(false);
+        expect(res.itemAdded).to.eql(false);
+        done();
+      });
+    });
+  });
+  context('should be able to chop wood and get item', function () {
+    beforeEach(function () {
+      sandbox.stub(db, 'getUserInfo').resolves({
+        name: 'main',
+        level: 1,
+        experience: 1,
+        chopLevel: 3,
+        chopExperience: 2,
+        items: {
+          wood: 1
+        }
+      });
+      sandbox.stub(db, 'updateUser').resolves(true);
+      sandbox.stub(Math, 'random').returns(1);
+    });
+    it('was able to chop wood and got item', function (done) {
+      chop.init().then(function (res) {
+        expect(res.success).to.eql(true);
+        expect(res.levelUp).to.eql(false);
+        expect(res.itemAdded).to.eql(true);
+        done();
+      });
+    });
+  });
+  context('should be able to chop wood and get item when user has none', function () {
+    beforeEach(function () {
+      sandbox.stub(db, 'getUserInfo').resolves({
+        name: 'main',
+        level: 1,
+        experience: 1,
+        chopLevel: 3,
+        chopExperience: 2,
+        items: {}
+      });
+      sandbox.stub(db, 'updateUser').resolves(true);
+      sandbox.stub(Math, 'random').returns(1);
+    });
+    it('was able to chop wood and got item', function (done) {
+      chop.init().then(function (res) {
+        expect(res.success).to.eql(true);
+        expect(res.levelUp).to.eql(false);
+        expect(res.itemAdded).to.eql(true);
+        done();
+      });
+    });
+  });
   context('should be able to level up from choping wood', function () {
     beforeEach(function () {
       sandbox.stub(db, 'getUserInfo').resolves({
